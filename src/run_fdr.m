5e53aa08c2ad4ab1c58c8f755b0141e7384cce4f
ishaanbatta <ishaanbatta@gmail.com>
2017-02-02 02:37:32 +0530

    debugged run_cc.m

--- src/run_fdr.m
+++ src/run_fdr.m
@@ -37,8 +37,7 @@ function run_fdr(input_dir_base,ROI_names,mask_file,output_dir_base)
         ROIs = strsplit(ROI_names);
             for rn=1:length(ROIs)
                 ROI_name = char(ROIs(rn));
-                
-                
+                disp(['Type of Map: ' type_of_map ', ROI: ' ROI_name]);
                 %Copy the Directory Structure of Pre-Processed Data
                 output_dir = strcat(output_dir_base,'/',ROI_name);
                 system(['mkdir -p ',output_dir]);
