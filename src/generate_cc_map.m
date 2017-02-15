87817772aef52cc6f1178c9187cf664df99d0eec
ishaanbatta <ishaanbatta@gmail.com>
2017-02-15 21:13:15 +0530

    Debugged global signal removal

--- src/generate_cc_map.m
+++ src/generate_cc_map.m
@@ -54,7 +54,7 @@ disp(' Removing the Global Signal..')
 size(RS_data)
 global_signal = nanmean(RS_data,1);
 size(global_signal)
-global_component = (global_signal*RS_data.')/(global_signal.'*global_signal) ;
+global_component = (global_signal*RS_data.')/(global_signal*global_signal.') ;
 RS_data = RS_data - global_component.'*global_signal;
 
 %normalise again
