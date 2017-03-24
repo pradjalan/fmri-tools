d03f433c384d00422b86c8561b576b3d9a36a7a9
ishaanbatta <ishaanbatta@gmail.com>
2017-03-25 02:33:29 +0530

    debugged

--- src/run_cc.m
+++ src/run_cc.m
@@ -52,7 +52,7 @@ function run_cc(input_dir_base,feat_dir_list,ROI_names,mask_thresholds,atlas,out
 %     while ischar(fline)
         fline = flines(cur_line);
         
-        scan_loc = [ input_dir_base '/' fline ];
+        scan_loc = strjoin(strcat(input_dir_base,'/',fline));
 %         fprintf(logfile,strcat('\nDoing Scan: ',scan_loc));
         disp(scan_loc);
         [~,c] = system(['ls -la ' scan_loc]);
