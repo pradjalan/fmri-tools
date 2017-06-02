054e505cd56564edcc74e1c5cbf9f5047099faeb
cyber <cyber@precuneous.cse.iitd.ernet.in>
2017-06-02 14:57:53 +0530

    added atlas to mask converter

--- src/atlas_to_masks.sh
+++ src/atlas_to_masks.sh
@@ -1,8 +1,7 @@
 # $1: input file, $2: min intensity $3: max intensity, $4: output dir; $5: Atlas prefix
 
-
+mkdir -p $4
 for i in `seq $2 $3`; do
 	echo $i
-	mkdir -p $4
-	fslmaths $1 -thr $i -uthr $i -bin $4/$i_$5.nii.gz
+	fslmaths $1 -thr $i -uthr $i -bin $4/$i.nii.gz
 done
