d8bebe7cf82f792c59477dc8d199530eaf4a5bc0
cyber <cyber@precuneous.cse.iitd.ernet.in>
2017-06-09 14:31:20 +0530

    added compute_roi_interesection

--- src/compute_roi_intersection.m
+++ src/compute_roi_intersection.m
@@ -8,21 +8,22 @@ if length(mask_file)==0
     mask_file = [fsldir,'/data/standard/MNI152_T1_2mm_brain_mask.nii.gz'];
 end
 
-rois = strsplit(ls(rois_dir));
+rois = strrep(strsplit(strtrim(ls(rois_dir))),'.nii.gz','');
 parameters = strsplit('TotalVoxels,NonZeroVoxels,Volume,Mean,Percentile100(Max),Percentile90,Percentile75,Percentile50(Median),Percentile25,Percentile10,Percentile0(Min),MeanPositive,MeanNegative',',');
 
 out_matrix = zeros(length(rois),length(rois), length(parameters));
 
 for r=1:length(rois)
-    cur_roi = char(rois(r));
-    input_file = [input_dir '/' cur_roi];
+    cur_roi = strrep(char(rois(r)),'.nii.gz','');
+    disp(cur_roi);
+    input_file = [input_dir '/' cur_roi '_logq_value0002.nii.gz'];
 % fid = fopen(out_file,'w');
 % fprintf(fid,'ROI_name,TotalVoxels,NonZeroVoxels,Volume,Mean,Percentile100(Max),Percentile90,Percentile75,Percentile50(Median),Percentile25,Percentile10,Percentile0(Min),MeanPositive,MeanNegative\n');
 
 
     for rn=1:length(rois)
-     roi = char(rois(rn));
-     roi_mask_path = [rois_dir '/' roi];
+     roi = strrep(char(rois(rn)),'.nii.gz','');
+     roi_mask_path = [rois_dir '/' roi '.nii.gz'];
 
      stat_command = ['fslstats ' roi_mask_path ' -V' ];
      [s,c] = system(['sh -c ". ${FSLDIR}/etc/fslconf/fsl.sh;${FSLDIR}/bin/' stat_command '  "']);
@@ -37,6 +38,7 @@ for r=1:length(rois)
      mul_neg_command = ['fslmaths ' input_file ' -uthr 0 -mul ' roi_mask_path ' ' rois_dir '/temp_blob_intersection_negative.nii.gz' ];
      mul_pos_command = ['fslmaths ' input_file ' -thr 0 -mul ' roi_mask_path ' ' rois_dir '/temp_blob_intersection_positive.nii.gz' ];
 
+     
      [s,~] =  system(['sh -c ". ${FSLDIR}/etc/fslconf/fsl.sh;${FSLDIR}/bin/' mul_command '  "']);
         if s~=0
             disp('Problem Creating ROI series:');
@@ -72,7 +74,7 @@ for r=1:length(rois)
      if s~=0
         disp('Error Running fslstats..');
      end
-     mat_line = [mat_line ',' strtrim(c)];
+     mat_line = [mat_line  str2num(strtrim(c))];
 
 
 
@@ -81,21 +83,36 @@ for r=1:length(rois)
      if s~=0
         disp('Error Running fslstats..');
      end
-     mat_line = [mat_line ',' strtrim(c)];
+     mat_line = [mat_line  str2num(strtrim(c))];
+     
 
      
      
      out_matrix(r,rn,:) = mat_line;
 %      fprintf(fid,[file_line '\n']);
-     delete('temp_blob_intersection*');
-
+     delete([rois_dir '/temp_blob_intersection*']);
+     
     end
 end
 % fclose(fid);     
-save([output_dir '/data_matrix.mat'],out_matrix); 
+
+if ~exist(output_dir,'dir')
+    mkdir(output_dir);
+end
+
+save([output_dir '/data_matrix.mat'],'out_matrix'); 
 
 
 %%%%%%%%%%%%%% USE imagesc(matrix) to plot 2D matrices...
 
+for p = 1:length(parameters)
+    figure('visible','off');
+    M = out_matrix(:,:,p);
+    imagesc(M.');
+    set(gca,'YtickLabel',rois);
+    title(parameters(p));
+    colorbar;
+    saveas(gca,[output_dir '/' char(parameters(p)) '.png']);
+end
 
 end
